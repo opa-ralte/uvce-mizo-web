89712773b321eaaafbc06bce45b5303563ea33c8
opa-ralte <nyletlaropa@gmail.com>
2025-07-22 20:24:34 +0530

    Importing the fs module to fix the reference error

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 app.get('/', (request, response) => {
 
-    readFile('./home.html', 'utf8', (err, html) => {
+    fs.readFile('./home.html', 'utf8', (err, html) => {
 
         if (err) {
             response.status(500).send('sorry, out of order');
