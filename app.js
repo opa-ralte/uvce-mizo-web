adfee4a7c4ded43290b6dd65c49f78914d525be2
opa ralte <67330990+opa-ralte@users.noreply.github.com>
2025-07-23 19:27:37 +0530

    Some ejs (#9)
    * Setting up codes for the homepage
    
    * There was a typo req in app.js which I changed to res
    
    * Landing page is in its early state
    
    * Organizing css better
    
    * Reorganizing files and errors rectified
    
    * change bgcolor to green
    
    * Revert the color back to antiquewhite
    
    * Adding pages for the footer links
    
    ---------
    
    Co-authored-by: remaralte <rema.ralte154@gmail.com>

--- app.js
+++ app.js
@@ -3,28 +3,103 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
-// custom modules
-
 const fs = require('fs');
-const bodyParser = require('body-parser');
+//👇put the connection to mongodb here
 
+//👆
+
+// custom modules👇
+
+// custom modules👆
+
+// This initializes an express application instance
 const app = express();
 
-app.get('/', (request, response) => {
 
-    fs.readFile('./home.html', 'utf8', (err, html) => {
+app.set('view engine', 'ejs'); // We can embed js directly within our html file
+app.use(express.urlencoded({urlencoded:true}));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/image', express.static(path.join(__dirname, 'image')));
 
-        if (err) {
-            response.status(500).send('sorry, out of order');
-        }
 
-        response.send(html);
 
-    });
+app.get('/home', async (req, res) => {
+    try {
+        res.render('index', { title: 'Home'});
+    } catch (error) {
+        res.status(500).send(`Something went wrong loading ${title}`);
+    }
+});
 
+app.get('/about', async (req, res) => {
+    try {
+        res.render('about', { title: 'About'});
+    } catch (error) {
+        res.status(500).send(`Something went wrong loading ${title}`);
+    } 
+});
+
+app.get('/alumni', async (req, res) => {
+    try {
+        res.render('alumni', { title: 'Alumni'});
+    } catch (error) {
+        res.status(500).send(`Something went wrong loading ${title}`);
+    }
+});
+
+app.get('/civil', async (req, res) => {
+    try {
+        res.render('civil', { title: 'Civil'});
+    } catch (error) {
+        res.status(500).send(`Something went wrong loading ${title}`);
+    }
+});
+
+app.get('/mech', async (req, res) => {
+    try {
+        res.render('mech', { title: 'Mech'});
+    } catch (error) {
+        res.status(500).send(`Something went wrong loading ${title}`);
+    }
+});
+
+app.get('/cse', async (req, res) => {
+    try {
+        res.render('cse', { title: 'CSE'});
+    } catch (error) {
+        res.status(500).send(`Something went wrong loading ${title}`);
+    }
+});
+
+app.get('/contacts', async (req, res) => {
+    try {
+        res.render('contacts', { title: 'Contacts'});
+    } catch (error) {
+        res.status(500).send(`Something went wrong loading ${title}`);
+    }
+});
+
+app.get('/extras', async (req, res) => {
+    try {
+        res.render('extras', { title: 'Extras'});
+    } catch (error) {
+        res.status(500).send(`Something went wrong loading ${title}`);
+    }
+});
+
+app.get('/danger', async (req, res) => {
+    try {
+        res.render('danger', { title: 'Danger'});
+    } catch (error) {
+        res.status(500).send(`Something went wrong loading ${title}`);
+    }
 });
 
 
-// Start the server
-app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`));
+
+
+// Start the server 🌐server start karo🌐
+app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000/home`));
 
