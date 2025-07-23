c178f2d5048f334ed8c76c7f1c19328668b473f2
opa-ralte <nyletlaropa@gmail.com>
2025-07-23 10:24:45 +0530

    Setting up codes for the homepage

--- app.js
+++ app.js
@@ -3,28 +3,34 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
-// custom modules
-
 const fs = require('fs');
-const bodyParser = require('body-parser');
+//👇put the connection to mongodb here
 
+//👆
+
+// custom modules👇
+
+// custom modules👆
+
+// This initializes an express application instance
 const app = express();
 
-app.get('/', (request, response) => {
+app.set('view engine', 'ejs'); // We can embed js directly within our html file
+app.set('layout', 'layouts/layout'); // This is the common skeleton of our webpage
+app.use(express.urlencoded({urlencoded:true}));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/image', express.static(path.join(__dirname, 'image')));
 
-    fs.readFile('./home.html', 'utf8', (err, html) => {
-
-        if (err) {
-            response.status(500).send('sorry, out of order');
-        }
-
-        response.send(html);
-
-    });
-
+app.get('/home', async (req, res) => {
+    try {
+        req.render('index', { title: 'Home Page'});
+    } catch (error) {
+        res.status(500).send('Something went wrong loading home page.');
+    }
 });
 
-
 // Start the server
-app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`));
+app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000/home`));
 
